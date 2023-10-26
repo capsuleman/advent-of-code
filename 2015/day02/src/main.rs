bf4b48ac676ea53e7a1cd1f4245f34b59bdf8679
Guillaume Vagner <guillaume.vagner@gmail.com>
2023-10-26 17:13:35 +0200

    ⭐⭐ Day 02 - 2015

--- 2015/day02/src/main.rs
+++ 2015/day02/src/main.rs
@@ -12,17 +12,17 @@ fn main() {
     let file = File::open(file_path).expect("File not found!");
     let buf_reader = BufReader::new(file);
 
-    let mut total_paper_length = 0;
+    let mut total_ribbon_length = 0;
 
     for line in buf_reader.lines() {
         let line = line.expect("read line");
 
         let (l, w, h) = parse_dimension(line);
 
-        total_paper_length += get_paper_length(l, w, h);
+        total_ribbon_length += get_ribbon_length(l, w, h);
     }
 
-    println!("{total_paper_length}");
+    println!("{total_ribbon_length}");
 }
 
 fn parse_dimension(line: String) -> (u32, u32, u32) {
@@ -35,12 +35,12 @@ fn parse_dimension(line: String) -> (u32, u32, u32) {
     (dimensions[0], dimensions[1], dimensions[2])
 }
 
-fn get_paper_length(l: u32, w: u32, h: u32) -> u32 {
-    let face_lw = l * w;
-    let face_lh = l * h;
-    let face_wh = w * h;
+fn get_ribbon_length(l: u32, w: u32, h: u32) -> u32 {
+    let ribbon_lw = l + w;
+    let ribbon_lh = l + h;
+    let ribbon_wh = w + h;
 
-    let min_face = min(min(face_lw, face_lh), face_wh);
+    let min_ribbon = min(min(ribbon_lw, ribbon_lh), ribbon_wh);
 
-    2 * face_lw + 2 * face_lh + 2 * face_wh + min_face
+    2 * min_ribbon + l * w * h
 }
