171a8d8c01230b6084da5557778f1e1749bb04d7
Guillaume Vagner <guillaume.vagner@gmail.com>
2023-11-01 10:03:29 +0100

    ⭐⭐ Day 14 - 2015

--- 2015/day14/src/main.rs
+++ 2015/day14/src/main.rs
@@ -1,6 +1,7 @@
 use lazy_static::lazy_static;
 use regex::Regex;
 use std::{
+    collections::{HashMap, HashSet},
     env,
     fs::File,
     io::{BufRead, BufReader},
@@ -16,6 +17,7 @@ lazy_static! {
 }
 
 struct Reindeer {
+    name: String,
     fly_speed: u32,
     fly_duration: u32,
     rest_duration: u32,
@@ -26,14 +28,21 @@ fn main() {
     let file_path = args.get(1).expect("to be given an input file.");
 
     let reindeers = parse_reindeers(&file_path);
-    let mut best_distance = 0;
 
-    for reindeer in reindeers {
-        let reindeer_distance = get_reindeer_distance(&reindeer, DURATION);
-        best_distance = u32::max(best_distance, reindeer_distance);
+    let mut reindeers_score = HashMap::new();
+    for reindeer in reindeers.iter() {
+        reindeers_score.insert(reindeer.name.clone(), 0);
     }
 
-    println!("{best_distance}");
+    for duration in 1..DURATION + 1 {
+        let best_reindeers = get_best_reindeers(&reindeers, duration);
+        for reindeer_name in best_reindeers {
+            reindeers_score
+                .entry(reindeer_name)
+                .and_modify(|score| *score += 1);
+        }
+    }
+    println!("{}", reindeers_score.values().max().unwrap());
 }
 
 fn parse_reindeers(file_path: &str) -> Vec<Reindeer> {
@@ -51,15 +60,36 @@ fn parse_reindeers(file_path: &str) -> Vec<Reindeer> {
 
 fn parse_reindeer(line: &str) -> Reindeer {
     let captures = INPUT_RE.captures(&line).expect("to parse line");
+    let name = captures[1].to_string();
     let fly_speed = captures[2].parse::<u32>().expect("a number");
     let fly_duration = captures[3].parse::<u32>().expect("a number");
     let rest_duration = captures[4].parse::<u32>().expect("a number");
 
     Reindeer {
+        name,
         fly_speed,
         fly_duration,
         rest_duration,
     }
+}
+
+fn get_best_reindeers(reindeers: &Vec<Reindeer>, duration: u32) -> HashSet<String> {
+    let mut best_reindeers = HashSet::new();
+    let mut best_distance = 0;
+
+    for reindeer in reindeers.iter() {
+        let reindeer_distance = get_reindeer_distance(reindeer, duration);
+
+        if reindeer_distance == best_distance {
+            best_reindeers.insert(reindeer.name.clone());
+        }
+        if reindeer_distance > best_distance {
+            best_reindeers = HashSet::from([reindeer.name.clone()]);
+            best_distance = reindeer_distance;
+        }
+    }
+
+    best_reindeers
 }
 
 fn get_reindeer_distance(reindeer: &Reindeer, duration: u32) -> u32 {
